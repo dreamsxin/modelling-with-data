164c54e0a94592a81e0761a3dcefa3a015145e4c
simongarisch <gatman946@gmail.com>
2018-01-14 16:25:59 +1100

    Adding 2.0 helloworld check

---
+++ Code/2.0 helloworld.c
@@ -0,0 +1,8 @@
+// Check your C environment by compiling and running "Hello World!"
+// a classic first program adapted from Kernighan & Ritchie (1988)
+
+#include <stdio.h>
+
+int main(){
+  printf("Hello World!\n");	
+}
