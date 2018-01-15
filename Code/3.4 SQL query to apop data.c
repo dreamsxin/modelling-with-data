5a468fe68a26df9106f62622ce741880a6e8a84e
simongarisch <gatman946@gmail.com>
2018-01-16 06:24:56 +1100

    SQL query to apop data

---
+++ Code/3.4 SQL query to apop data.c
@@ -0,0 +1,21 @@
+//gcc "3.4 SQL query to apop data.c" -std=gnu99 -lapophenia -lgsl -lgslcblas -lsqlite3
+//./a.out
+
+#include <apop.h>
+
+void percap(gsl_vector *in){
+  double gdp_per_cap  = gsl_vector_get(in, 1)/gsl_vector_get(in, 0);
+  gsl_vector_set(in, 2, gdp_per_cap); //column 2 is gdp_per_cap.
+}
+
+int main(){
+  apop_db_open("data-wb.db");
+  apop_opts.db_name_column = "country";
+  apop_data *d   = apop_query_to_data("select pop.country as country, \
+    pop.population as pop, gdp.GDP as GDP, 1 as GDP_per_cap\
+    from pop, gdp \
+    where pop.country == gdp.country");
+  apop_matrix_apply(d->matrix, percap);
+  apop_data_show(d);
+  apop_data_print(d, "wbtodata_output", .output_type='d');
+}
