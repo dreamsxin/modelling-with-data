9125855503ce9b1d08bea0385b3c446451170c86
simongarisch <gatman946@gmail.com>
2018-01-21 07:22:41 +1100

    Precision example

---
+++ Code/4.12 fuzz.c
@@ -0,0 +1,14 @@
+//gcc "4.12 fuzz.c"
+//./a.out
+//bear in mind floating point precision
+
+#include <stdio.h>
+
+int main(){
+  double t = 1.2;
+  t -= 0.4;
+  t -= 0.4;
+  t -= 0.4;
+  if (t<0)
+    printf ("By the IEEE floating-point standard, 1.2 - 3*0.4 < 0.\n");
+}
