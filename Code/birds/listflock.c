73afb6a3a672e79f90a77cffb54bfaca27f0ae7e
simongarisch <gatman946@gmail.com>
2018-01-29 06:54:48 +1100

    Modified listflock

--- Code/birds/listflock.c
+++ Code/birds/listflock.c
@@ -1,3 +1,11 @@
+//sudo apt-get install libglib2.0-dev
+//locate glib.h
+//pkg-config --cflags --libs glib-2.0
+//outputs... -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -lglib-2.0
+
+//gcc "birds.c" "listflock.c" -std=gnu99 -lapophenia -lgsl -lgslcblas -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -lglib-2.0
+//./a.out
+
 #include "birds.h"
 #include <glib.h>
 
@@ -9,34 +17,39 @@ void flock_plays(){ g_list_foreach(flock, bird_plays, NULL); }
 void add_to_flock(bird* b){ flock = g_list_prepend(flock, b); }
 
 void free_bird(bird* b){
-    flock   = g_list_remove(flock, b);
-    free(b); }
+  flock   = g_list_remove(flock, b);
+  free(b);
+}
 
 bird * find_opponent(int n){ return g_list_nth_data(flock, n); }
 
 void wealth_foreach(void *in, void *total){
-    *((int*)total)   += ((bird*)in)->wealth;  }
+  *((int*)total)   += ((bird*)in)->wealth;
+}
 
 int flock_wealth(){
-  int   total   = 0;
-    g_list_foreach(flock, wealth_foreach, &total);
-    return total; }
+  int total = 0;
+  g_list_foreach(flock, wealth_foreach, &total);
+  return total;
+}
 
 int flock_size(){ return g_list_length(flock); }
 
 void bird_count(void *in, void *v){
   bird *b   = in;
-    if (b->type == 'h')
-        hawks   ++;
-    else doves   ++;
+  if (b->type == 'h')
+    hawks++;
+  else
+    doves++;
 }
 
 double count(int period){
   int   total_wealth    =flock_wealth();
-    hawks   = doves   = 0;
-    g_list_foreach(flock, birth_or_death, &total_wealth);
-    g_list_foreach(flock, bird_count, NULL);
-    printf("%i\t%i\t%i\n", period, hawks, doves);
-    return (doves+0.0)/hawks;}
+  hawks   = doves   = 0;
+  g_list_foreach(flock, birth_or_death, &total_wealth);
+  g_list_foreach(flock, bird_count, NULL);
+  printf("%i\t%i\t%i\n", period, hawks, doves);
+  return (doves+0.0)/hawks;
+}
 
 void flock_init(){ flock  = NULL; }
